36156a9f9ba23d68a81bc2e7d4f9e830b0ccd61a
gpedelagrabe <gpedelagrabe@gmail.com>
2019-01-01 20:08:32 +0100

    setPositions.

--- Model/grid.c
+++ Model/grid.c
@@ -29,7 +29,6 @@ void setNumberPositions(
             x = 0;
             continue;
         }
-        // printf("%d, %d : %c\n", x, y, currentCharacter);
 
         switch (currentCharacter) {
             case '#': // wall
@@ -65,6 +64,84 @@ void setNumberPositions(
     }
 }
 
+void setPositions(
+        char* filename,
+        Position** walls,
+        Position** grounds,
+        Position** stands,
+        Position** blocks
+        )
+{
+    FILE* file;
+    file = fopen(filename, "r");
+
+    int wallsIndice = 0;
+    int groundsIndice = 0;
+    int standsIndice = 0;
+    int blocksIndice = 0;
+
+
+    int currentCharacter = 0;
+    int x = 0;
+    int y = 0;
+
+    while (EOF != currentCharacter) {
+        currentCharacter = fgetc(file);
+        if ('\n' == currentCharacter || EOF == currentCharacter) {
+            y++;
+            x = 0;
+            continue;
+        }
+
+        Position position = {x, y};
+
+        switch (currentCharacter) {
+            case '#': // wall
+                walls[wallsIndice] = &position;
+                wallsIndice++;
+                break;
+            case ' ': // ground
+                grounds[groundsIndice] = &position;
+                groundsIndice++;
+                break;
+            case '.': // stand on ground
+                stands[standsIndice] = &position;
+                standsIndice++;
+                grounds[groundsIndice] = &position;
+                groundsIndice++;
+                break;
+            case '$': // block on ground
+                blocks[blocksIndice] = &position;
+                blocksIndice++;
+                grounds[groundsIndice] = &position;
+                groundsIndice++;
+                break;
+            case '@': // sokoban on ground
+                grounds[groundsIndice] = &position;
+                groundsIndice++;
+                break;
+            case '*': // block on stand on ground
+                blocks[blocksIndice] = &position;
+                blocksIndice++;
+                stands[standsIndice] = &position;
+                standsIndice++;
+                grounds[groundsIndice] = &position;
+                groundsIndice++;
+                break;
+            case '+': // sokoban on stand on ground
+                stands[standsIndice] = &position;
+                standsIndice++;
+                grounds[groundsIndice] = &position;
+                groundsIndice++;
+                break;
+            default:
+                break;
+        }
+
+        x++;
+    }
+}
+
 void grid_init(
         char* filename
         )
@@ -80,5 +157,12 @@ void grid_init(
     printf("Nombre de grounds : %d.\n", numberGrounds);
     printf("Nombre de blocks : %d.\n", numberBlocks);
     printf("Nombre de stands : %d.\n", numberStands);
+
+    Position** walls = malloc(numberWalls * sizeof(Position));
+    Position** grounds = malloc(numberGrounds * sizeof(Position));
+    Position** stands = malloc(numberStands * sizeof(Position));
+    Position** blocks = malloc(numberBlocks * sizeof(Position));
+
+    setPositions(filename, walls, grounds, stands, blocks);
 
 }
--- main.c
+++ main.c
@@ -14,7 +14,6 @@ int main()
 
 
 
-
     free(filename);
 
     return EXIT_SUCCESS;
